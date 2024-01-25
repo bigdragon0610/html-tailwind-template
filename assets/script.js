344c7d003a1ddab1c62ecb4348683b50bf40ca6d
ryudai <bigdragon.06102001@gmail.com>
2024-01-25 18:29:56 +0900

    Alpine.jsを追加

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-const title = document.getElementById('title')
-const button = document.getElementById('button')
-
-button.addEventListener('click', () => {
-  title.classList.toggle('text-blue-500')
-  title.classList.toggle('text-blue-300')
-})
